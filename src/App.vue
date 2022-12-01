fd3911c73482dc09cec9d22a5f04ed731151b5cd
Alex <florinalexandru.tita@ulbsibiu.ro>
2022-12-01 14:54:06 +0200

    Vuetify support

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,59 @@
-<script setup>
-import TheStats from "./components/TheStats.vue";
+<script>
+import Home from "./Home.vue";
+import Stats from "./Stats.vue";
+import NotFound from "./404.vue";
+import { useTheme } from "vuetify";
+const routes = {
+  "/": Home,
+  "/stats": Stats,
+};
+
+export default {
+  setup() {
+    const theme = useTheme();
+
+    return {
+      theme,
+      toggleTheme: () =>
+        (theme.global.name.value = theme.global.current.value.dark
+          ? "light"
+          : "dark"),
+    };
+  },
+  data() {
+    return {
+      currentPath: window.location.hash,
+    };
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || "/"] || NotFound;
+    },
+  },
+  mounted() {
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+    });
+  },
+  methods: {},
+};
 </script>
 
 <template>
   <header><h1>Statistici</h1></header>
 
   <main>
-    <TheStats />  </main>
+    <main>
+      <v-app>
+        <v-app-bar title="My Deck">
+          <v-btn href="#/" icon="mdi-home"></v-btn>
+          <v-btn href="#/stats" icon="mdi-cards"></v-btn>
+          <v-btn @click="toggleTheme" icon="mdi-animation"></v-btn>
+        </v-app-bar>
+        <v-main>
+          <component :is="currentView" />
+        </v-main>
+      </v-app>
+    </main>
+  </main>
 </template>
--- src/components/TheStats.vue
+++ src/components/TheStats.vue
@@ -1,11 +1,28 @@
 <script>
-
+export default {
+  data() {
+    return {
+      allArtists: null,
+    };
+  },
+  methods: {
+    fetchArtists() {
+      const apiKey = "5732de1113ade3d82c6082664921fbdc";
+      const Url = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=romania&api_key=${apiKey}&format=json`;
+      fetch(Url)
+        .then((response) => response.json())
+        .then((data) => {
+          this.allArtists = data;
+        });
+    },
+  },
+};
 </script>
 
-<template> 
-    <h2> DA </h2>
+<template>
+  <h2>DA</h2>
+  <button @click="fetchArtists">Get my Artists</button>
 </template>
-
 
 <style scoped>
 img {
